7c9670334bb6668e43dcee71284ef31ca46efb9b
kassimii <patyboc@yahoo.com>
2021-04-27 22:52:51 +0300

    added reCAPTCHA verification for reset password request

--- frontend/src/pages/ForgotPasswordPage.js
+++ frontend/src/pages/ForgotPasswordPage.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import { TextField, InputLabel } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
+import Recaptcha from 'react-recaptcha';
 import Message from '../components/Message';
 import FormContainer from '../components/FormContainer';
 import { sendResetPasswordLink } from '../redux/actions/userActions';
 import { USER_FORGOT_PASSWORD_RESET } from '../redux/constants/userConstants';
 
+const { REACT_APP_RECAPTCHA_SITE_KEY } = process.env;
+
 const ForgotPasswordPage = ({ history }) => {
   const [email, setEmail] = useState('');
+  const [isHuman, setIsHuman] = useState(false);
+  const [message, setMessage] = useState('');
+  const [emptyFieldError, setEmptyFieldError] = useState({});
 
   const dispatch = useDispatch();
 
@@ -25,17 +32,47 @@ const ForgotPasswordPage = ({ history }) => {
 
     return function resetState() {
       dispatch({ type: USER_FORGOT_PASSWORD_RESET });
+      setMessage('');
     };
   }, [history, userInfo, dispatch]);
 
+  const validateForm = () => {
+    let temp = {};
+    temp.email = /\S+@\S+\.\S+/.test(email)
+      ? ''
+      : 'Adresa de email nu este validă';
+
+    setEmptyFieldError({ ...temp });
+
+    return Object.values(temp).every((x) => x === '');
+  };
+
+  const recaptchaLoaded = () => {
+    console.log('reCAPTCHA successfully loaded');
+  };
+
+  const verifyCallback = (response) => {
+    if (response) {
+      setIsHuman(true);
+    }
+  };
+
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(sendResetPasswordLink(email));
+    if (isHuman) {
+      if (validateForm()) {
+        setMessage('');
+        dispatch(sendResetPasswordLink(email));
+      }
+    } else {
+      setMessage('Vă rugăm să confirmați că sunteți o persoană reală.');
+    }
   };
 
   return (
     <FormContainer>
       <h1>Modificare parolă</h1>
+      {message && <Message variant='danger'>{message}</Message>}
       {resetError && (
         <Message variant='danger'>
           Nu există un utilizator cu această adresă de email.
@@ -45,18 +82,31 @@ const ForgotPasswordPage = ({ history }) => {
         <Message variant='success'>Verificați-vă emailul!</Message>
       )}
       <Form onSubmit={submitHandler}>
-        <p>
-          Veți primi un link de resetare a parolei pe adresa de mail introdusă.
-        </p>
+        <InputLabel>
+          Veți primi un link de resetare a parolei pe adresa de email introdusă.
+        </InputLabel>
+
         <Form.Group controlId='email'>
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type='email'
-            placeholder='Introduceti email'
+          <TextField
+            required
+            variant='outlined'
+            label='Adresă email'
+            fullWidth
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
+            {...(emptyFieldError.email && {
+              error: true,
+              helperText: emptyFieldError.email,
+            })}
+          />
         </Form.Group>
+
+        <Recaptcha
+          sitekey={REACT_APP_RECAPTCHA_SITE_KEY}
+          render='explicit'
+          onloadCallback={recaptchaLoaded}
+          verifyCallback={verifyCallback}
+        />
 
         <Button type='submit' variant='dark'>
           Resetează
