7accf567abaf092dee79f08cb4151e1fc9c5d673
kassimii <patyboc@yahoo.com>
2021-05-30 18:43:27 +0300

    added authorization for jest tests

--- backend/tests/routes.test.js
+++ backend/tests/routes.test.js
@@ -1,16 +1,22 @@
-const request = require('supertest');
-const app = require('../server');
+import request from 'supertest';
+import app from '../server.js';
+import generateToken from '../utils/generateToken.js';
+
+const prelevationToken = generateToken('5fd8acfcb2f3e44014453c96');
+const bearerToken = `Bearer ${prelevationToken}`;
 
 describe('Post Patient', () => {
   it('should create a new patient', async (done) => {
-    const res = await request(app).post('/api/patients').send({
-      name: 'Ionica',
-      surname: 'Doe Test',
-      cnp: '6203184124578',
-      addressID: 'Stejarilor',
-      phoneNumber: '0767676767',
-    });
-    group_id = res.body.group.id;
+    const res = await request(app)
+      .post('/api/patients')
+      .set('Authorization', bearerToken)
+      .send({
+        name: 'Ionica',
+        surname: 'Doe Test',
+        cnp: '5000310351578',
+        addressID: 'Stejarilor',
+        phoneNumber: '0767676767',
+      });
     expect(res.statusCode).toEqual(201);
     done();
   });
